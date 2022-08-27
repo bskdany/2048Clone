db69fc253884d4323005caaec38f888b040692a2
bskdany <danylobyshkin@gmail.com>
2022-08-27 13:49:08 -0400

    mobile website controls

--- 2048Clone/Assets/Scripts/GameManager.cs
+++ 2048Clone/Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Net.WebSockets;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -21,6 +20,10 @@ public class GameManager : MonoBehaviour
     public GG ggScript;
 
     public GameObject boardManagerObject;
+
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
 
     List<GameObject> tileList = new List<GameObject>();
     List<GameObject> tileListCopy = new List<GameObject>();
@@ -112,7 +115,7 @@ public class GameManager : MonoBehaviour
     void takeInput()
     {
         if (Input.GetKeyDown(KeyCode.D))
-        {   
+        {
             directionOfMovement = "Right";
             canTakeInput = false;
         }
@@ -130,6 +133,57 @@ public class GameManager : MonoBehaviour
         {
             directionOfMovement = "Down";
             canTakeInput = false;
+        }
+
+        //swipe detection code I stole
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            directionOfMovement = "Right";
+                            canTakeInput = false;
+                        }
+                        else
+                        {   //Left swipe
+                            directionOfMovement = "Left";
+                            canTakeInput = false;
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            directionOfMovement = "Up";
+                            canTakeInput = false;
+                        }
+                        else
+                        {   //Down swipe
+                            directionOfMovement = "Down";
+                            canTakeInput = false;
+                        }
+                    }
+                }
+            }
         }
     }
    
